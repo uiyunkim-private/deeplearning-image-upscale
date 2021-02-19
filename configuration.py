2a4e8a50acd8076eb8fc1f06af70435cac8f1f9e
Uiyun Kim <66242193+uiyunkim-git@users.noreply.github.com>
2021-02-19 13:58:14 +0900

    Delete configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-import os
-from pathlib import Path
-
-DIR_DATASETS = Path('/Volumes/GoogleDrive/My Drive/storage/video/porn/pornhub')
-DIR_PROCESSED = Path('/Volumes/GoogleDrive/My Drive/storage/dataset/dlss/pornhub_x4')
